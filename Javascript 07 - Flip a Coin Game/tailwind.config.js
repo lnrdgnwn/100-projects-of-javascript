3d77a645414e36c47d6ee2a3e6a4da56ee7236c5
lnrdgnwn <leonardogunawan15@gmail.com>
2024-09-27 20:21:07 +0700

    update index.html and script.js

--- Javascript 07 - Flip a Coin Game/tailwind.config.js
+++ Javascript 07 - Flip a Coin Game/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html", "./src/**/*.{js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-tails': 'spin-tails 3s forwards',
+        'spin-heads': 'spin-heads 3s forwards',
+      },
+      keyframes: {
+        'spin-tails': {
+          '0%': { transform: 'rotateX(0deg)' },
+          '100%': { transform: 'rotateX(1980deg)' },
+        },
+        'spin-heads': {
+          '0%': { transform: 'rotateX(0deg)' },
+          '100%': { transform: 'rotateX(2160deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
